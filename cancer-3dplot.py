231d9081c9b90135fb3ca56a581121e30501c554
Luis Lugo <ogullius@gmail.com>
2024-05-05 10:14:59 -0600

    Export notebook to python file

---
+++ cancer-3dplot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# %%
+from mpl_toolkits.mplot3d import Axes3D
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17) 
+  
+# data (as pandas dataframes) 
+X = breast_cancer_wisconsin_diagnostic.data.features 
+y = breast_cancer_wisconsin_diagnostic.data.targets 
+
+
+# %%
+colorMap = {"M":"r","B":"b"}
+fig = plt.figure(figsize=(18,15))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X['radius1'],
+           X['texture1'],
+           X['perimeter1'],
+           c=y['Diagnosis'].map(colorMap)
+           )
+
+ax.set_xlabel("radius1")
+ax.set_ylabel("texture1")
+ax.set_zlabel("perimiter1")
+plt.show()
+
+
